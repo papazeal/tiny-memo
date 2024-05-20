5ae51016dfd9100ac356dd43c9662e4b71a72f78
papazeal <zeally7@gmail.com>
2024-05-20 21:37:54 +0700

    preload sfx

--- game.gd
+++ game.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 @onready var line:Line2D = $Line2D
 @onready var audio:AudioStreamPlayer2D = $AudioStreamPlayer2D
-
+var TapSound = preload("res://tap.mp3")
 var Dot = preload("res://dot.tscn")
 var selected_dots = []
 var dots = []
-var colors_set = [Color.ORANGE, Color.YELLOW_GREEN]
+var colors_set = [Color.ORANGE, Color.YELLOW_GREEN, Color.LIGHT_CORAL]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	audio.stream = TapSound
 	init_level()
 	pass # Replace with function body.
 
@@ -19,13 +20,20 @@ func _process(delta):
 	pass
 	
 func init_level():
-	var colors_set = colors_set + colors_set
-	colors_set.shuffle()
+	# pallet
+	var pallet = colors_set
+	pallet = pallet+pallet
+	pallet.shuffle()
+	
+	var rng = RandomNumberGenerator.new()
 	var i = 0
+	
 	for p in line.points:
 		print_debug(p)
+		#p.x += 20-40*rng.randf()
+		#p.y += 20-40*rng.randf()
 		var d:Dot = Dot.instantiate()
-		d.init(colors_set[i])
+		d.init(pallet[i])
 		d.index = i
 		d.position = p
 		d.connect('click', _on_dot_click)
@@ -53,8 +61,6 @@ func _on_dot_click(dot:Dot):
 		#check color
 		if selected_dots[0].dot_color == selected_dots[1].dot_color:
 			print('match!')
-			#selected_dots[0].queue_free()
-			#selected_dots[1].queue_free()
 			selected_dots[0].disclose()
 			selected_dots[1].disclose()
 			audio.pitch_scale = 1.4
