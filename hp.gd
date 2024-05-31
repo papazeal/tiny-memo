8e6aecb4d58b1ab6683de1c9b36a07bbf2ef9760
papazeal <zeally7@gmail.com>
2024-05-31 16:30:15 +0700

    up up

--- hp.gd
+++ hp.gd
@@ -2,18 +2,20 @@ extends Node2D
 class_name HpBar
 
 var padding = 24
-var image = Image.load_from_file("res://dot.svg")
-var texture = ImageTexture.create_from_image(image)
 var dots = []
+var Dot = preload("res://dot.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in 10:
-		var dot:Sprite2D = Sprite2D.new()
-		dot.texture = texture
-		dot.scale = Vector2(0.1,0.1)
-		dot.position = Vector2(i*padding, 0)
-		dots.append(dot)
-		add_child(dot)
+		#var dot:Sprite2D = Sprite2D.new()
+		#dot.texture = texture
+		#dot.scale = Vector2(0.1,0.1)
+		#dot.position = Vector2(i*padding, 0)
+		var d:Dot = Dot.instantiate()
+		d.position = Vector2(i*padding, 0)
+		#d.scale = Vector2(0.1,0.1)
+		dots.append(d)
+		add_child(d)
 	pass # Replace with function body.
 
 
